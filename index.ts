9c0fca5344282428c245ace1a7579df50392971e
Luke Chatton <chattonluke@gmail.com>
2018-03-15 10:57:32 -0700

    convert to material

--- index.ts
+++ index.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { MarkdownEditorComponent } from './md-editor/md-editor.component';
 
 export * from './md-editor/md-editor.component';
 
 @NgModule({
-  declarations: [
-    MarkdownEditorComponent
-  ],
-  imports: [
-    CommonModule,
-    FormsModule
-  ],
-  exports: [
-    MarkdownEditorComponent
-  ]
+    declarations: [
+        MarkdownEditorComponent
+    ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        MatButtonModule, MatIconModule, FlexLayoutModule
+    ],
+    exports: [
+        MarkdownEditorComponent
+    ]
 })
 export class LMarkdownEditorModule { }
